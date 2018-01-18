8b13f73a1883e2ce723826cdd362dccea036d215
Andrei Tamas <andrei.tamas101@gmail.com>
2018-01-18 16:01:42 +0200

    added moment.js usage examples

--- src/app/week-view-wrapper/week-nav.component.ts
+++ src/app/week-view-wrapper/week-nav.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDatepickerInputEvent } from '@angular/material';
+import * as moment from 'moment';
 
 @Component({
   selector: 'nav-component',
@@ -11,7 +12,16 @@ export class WeekNavComponent {
   public dateDisplay = new Date();
   public selectedDate: string[] = [];
 
-  addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
-    this.selectedDate.push(`${type}: ${event.value}`);
+  addEvent(event: MatDatepickerInputEvent<Date>) {
+    const date = event.value;
+    console.log('selected date', date);
+    console.log('selected week', moment(date).isoWeek());
+    console.log('this week', moment().isoWeek());
+
+    console.log('selected Monday', moment(date).isoWeekday(1));
+    console.log('selected Tuesday', moment(date).isoWeekday(2));
+    console.log('selected wed', moment(date).isoWeekday(3));
+    console.log('selected thur', moment(date).isoWeekday(4));
+    console.log('selected fri', moment(date).isoWeekday(4));
   }
 }
