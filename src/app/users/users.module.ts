e589fb9d999a8960d5e077839cfad9ddf7aaeb14
IuliaV <iuliamvoicu@yahoo.com>
2018-01-19 11:33:23 +0200

    renamed the component

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,23 +4,13 @@ import { NtmaterialModule } from '../ntmaterial/ntmaterial.module';
 import { UsersComponent } from './users-list.component';
 import { UsersService } from './users.service';
 
-import { UserModifierComponent } from '../user-modifier/user-dialog.component';
+import { UserCrudBtn } from '../user-modifier/user-crud-button.component';
 import { DialogWrapper } from '../user-modifier/dialog-wrapper.component';
 import { UserModifierForm } from '../user-modifier/edit-user-form.component';
 @NgModule({
-  declarations: [
-    UsersComponent,
-    UserModifierComponent,
-    DialogWrapper,
-    UserModifierForm
-  ],
-  exports: [
-    UsersComponent,
-    UserModifierComponent,
-    DialogWrapper,
-    UserModifierForm
-  ],
-  entryComponents: [UserModifierComponent, DialogWrapper, UserModifierForm],
+  declarations: [UsersComponent, UserCrudBtn, DialogWrapper, UserModifierForm],
+  exports: [UsersComponent, UserCrudBtn, DialogWrapper, UserModifierForm],
+  entryComponents: [UserCrudBtn, DialogWrapper, UserModifierForm],
   imports: [NtmaterialModule],
   providers: [UsersService]
 })
