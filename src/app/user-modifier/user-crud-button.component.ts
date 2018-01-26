fd15939a55e42394cb7accc5737b08d7de57649b
SebastianP <sebpopovici@yahoo.com>
2018-01-26 16:02:45 +0200

    made dialog box responsive

--- src/app/user-modifier/user-crud-button.component.ts
+++ src/app/user-modifier/user-crud-button.component.ts
@@ -4,6 +4,9 @@ import { UpdateUserComponent } from './dialog-update-user.component';
 import 'rxjs/add/operator/filter';
 import { EventEmitter } from '@angular/core';
 
+import { ObservableMedia, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs/Subscription';
+
 @Component({
   selector: 'user-crud-button',
   template: `<button mat-raised-button (click)="openDialog()">{{btnText || "Please specify the btnText"}}</button>`
@@ -16,8 +19,9 @@ export class UserCrudBtn {
   constructor(public dialog: MatDialog) {}
 
   openDialog(): void {
-    let dialogRef = this.dialog.open(UpdateUserComponent, {
-      width: '50vw',
+    let dialogRef = this.dialog.open(UpdateUserComponent, { 
+      // // MediaQuery activation changes
+      // width: `${this.dialogWidth}vw`,
       data: this.selectedUser ? this.selectedUser : {}
     });
 
@@ -29,4 +33,25 @@ export class UserCrudBtn {
         this.deliverUpdatedUser.emit(result);
       });
   }
-}
+//   // MediaQuery activation changes
+
+//   public dialogWidth: number;
+//   watcher: Subscription;
+//   constructor(media: ObservableMedia, public dialog: MatDialog) {
+//     this.watcher = media.subscribe((change: MediaChange) => {
+//       if (change.mqAlias === 'xs') {
+//         console.log('dimensiune noua:', change.mqAlias);
+//         this.switchDialogWidth(100);
+//       } else {
+//         console.log('dimensiune noua:', change.mqAlias);
+//         this.switchDialogWidth(50);
+//       }
+//     });
+//   }
+//   ngOnDestroy() {
+//     this.watcher.unsubscribe();
+//   }
+//   switchDialogWidth(width) {
+//     this.dialogWidth = width;
+//   }
+// }
