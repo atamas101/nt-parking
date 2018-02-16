cc69531cae710db5dd81279ff8a71b2763e4df59
Andrei Tamas <andrei.tamas101@gmail.com>
2018-02-16 16:25:47 +0100

    fix user dates & slot styles

--- src/app/week/day-component/subscribers-list.component.ts
+++ src/app/week/day-component/subscribers-list.component.ts
@@ -1,16 +1,29 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { Moment } from 'moment';
+import * as moment from 'moment';
 
 @Component({
   selector: 'subscribers-list',
   templateUrl: 'subscribers-list.html',
   styleUrls: ['./waiting-list-content.scss']
 })
-export class SubscribersListComponent {
+export class SubscribersListComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<SubscribersListComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
+  ngOnInit() {
+    this.data.people = this.data.people.map(item => {
+      item.hireDate = moment(item.hireDate)
+        .utc()
+        .startOf('day')
+        .add(12, 'hour')
+        .toDate();
+
+      return item;
+    });
+  }
 
   onClose(): void {
     this.dialogRef.close();
