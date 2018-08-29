8cd601337d18d223ff3aee64751f971eafcfef80
Andrei Tamas <andrei.tamas101@gmail.com>
2018-08-29 09:42:57 +0300

    fix mongo server connection settings

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const mongoose = require('mongoose');
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
+const mongoOpts = {
+  poolSize: 20,
+  reconnectTries: Number.MAX_SAFE_INTEGER,
+  socketTimeoutMS: 480000,
+  keepAlive: 300000
+};
 // Connect to our Database and handle an bad connections
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(
+  process.env.DATABASE,
+  mongoOpts
+);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', err => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
